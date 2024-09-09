e9cc762aac9931edf4312cb428f26138e9536558
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2024-09-08 21:20:45 -0300

    Publish config
    Revert "Heroku test"
    
    This reverts commit 7bf7f346a8ab542c27f8533a85678c8cce68ac91.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,15 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.dokka) apply true
+    alias(libs.plugins.ktor.plugin) apply true
     alias(libs.plugins.serialization)
     id("io.lb.dokka") apply true
     id("io.lb.jacoco.multi-module")
+}
+
+application {
+    mainClass.set("io.lb.middleware.core.MiddlewareApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
